5015f5ab38d163037b03153df9bd5bbf4262e7c3
RayaneOliveira <rayoliveira006@gmail.com>
2016-02-17 17:50:53 -0200

    Atualizando

---
+++ ExercicioJSF2/src/br/edu/ifpb/Entidades/Usuario.java
@@ -0,0 +1,35 @@
+package br.edu.ifpb.Entidades;
+
+public class Usuario {
+	
+	private String nome, data;
+	private int matricula;
+
+	public Usuario() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	
+}
---
+++ ExercicioJSF2/src/br/edu/ifpb/bean/UsuarioBean.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb.bean;
+
+import br.edu.ifpb.Entidades.Usuario;
+
+public class UsuarioBean {
+
+	public UsuarioBean() {
+		
+	}
+
+}
